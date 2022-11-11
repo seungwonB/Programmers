d6f5d9a96a24003ae25f2b2df0a4af180010efa3
Seungwon Bang <73030613+seungwonB@users.noreply.github.com>
2022-11-11 22:42:33 +0900

    월코챌3

--- LV2/n^2 배열 자르기.py
+++ LV2/n^2 배열 자르기.py
@@ -3,10 +3,10 @@ def solution(n, left, right):
 
     if left-n < 0:
         i = 0
-        j = left % n
     else:
-        i = int((left-n)/n) + 1
-        j = left % n
+        i = int(left/n)
+        
+    j = left % n
 
     while left != right+1:
         arr.append(max(i, j) + 1)
